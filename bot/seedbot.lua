f7854398433c14ea7627d96525b8589546a0a5a3
tele911 <911a.a.m.v2@gmail.com>
2016-03-03 21:10:10 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {112693322,216489789,73767162,0,tonumber(our_id)},--Sudo users
+    sudo_users = {112693322,73979927,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
